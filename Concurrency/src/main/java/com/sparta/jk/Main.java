fcd250ce91ec4d48f8c4a0d45dd2524911668620
JayenKerai <32294741+JayenKerai@users.noreply.github.com>
2020-01-26 18:51:03 +0000

    Add files via upload

--- Concurrency/src/main/java/com/sparta/jk/Main.java
+++ Concurrency/src/main/java/com/sparta/jk/Main.java
@@ -1,21 +1,21 @@
 package com.sparta.jk;
 
 public class Main {
-    public static void main(String[] args) {
-        //using 1st method to create thread
+    public static void main(String[] args) throws InterruptedException {
+        //using 1st method to create thread 1
         Thread thread1 = new Thread(new SharedCounter());
 
-        //using 2nd method to create thread
+        //using 2nd method to create thread 2
         SharedCounter sC = new SharedCounter();
         Thread thread2 = new Thread(sC);
 
-        //using 1st method to create thread
+        //using 1st method to create thread 3
         Thread thread3 = new Thread(new SharedCounter());
 
         thread1.setName("thread 1");
         thread2.setName("thread 2");
         thread3.setName("thread 3");
-        
+
         //run both threads
         thread1.start();
         thread2.start();
@@ -36,7 +36,10 @@ class SharedCounter implements Runnable {
         return counter;
     }
 
-    public void incrementCounter() {
-        ++counter;
+    public synchronized void incrementCounter() {
+        for (int i = 0; i < 1000; i++) {
+            System.out.println(counter + " " + Thread.currentThread().getName()); //prints current counter value and its thread
+            ++counter;
+        }
     }
 }
